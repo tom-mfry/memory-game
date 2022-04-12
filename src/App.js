5000c05b25b1193f96f2e9786f1489e3b0c47e6b
Tom Fry <tom.mfry@gmail.com>
2022-04-12 16:32:22 +0100

    initialised deck of 12 cards. created shuffle function. identified 'turns' state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages] // creat duplicate cards
+      .sort(() => Math.random() - 0.5) // random shuffle - <0 swap, >0 stay
+      .map((card) => ({ ...card, id: Math.random() })); // add id attribute to object
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
